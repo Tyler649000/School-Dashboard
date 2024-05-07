441a5f3c45a9846204f0f60445b10243d9f0d270
Tyler649000 <134805689+Tyler649000@users.noreply.github.com>
2024-05-07 09:50:15 -0400

    Update drag-and-drop.js

--- drag-and-drop.js
+++ drag-and-drop.js
@@ -2,11 +2,10 @@ const homeworkItems = document.querySelectorAll('.homework');
 const homeworkContainer = document.querySelector('#homework-container');
 
 document.addEventListener('dragover', (event) => {
-    console.log(document.getElementById('homework-container').scrollHeight);
     const draggingElement = homeworkContainer.querySelector('.dragging');
     const siblings = [...homeworkContainer.querySelectorAll('.homework:not(.dragging)')];
     const nextSibling = siblings.find(sibling => {
-        return event.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+        return event.clientY + document.getElementById('homework-container').scrollTop <= sibling.offsetTop + sibling.offsetHeight / 2;
     });
     homeworkContainer.insertBefore(draggingElement, nextSibling);
     event.preventDefault();
